7f52943f876949d788571fd8c690278f4bf0c2a8
ChoiGoun <gowoonvv@gmail.com>
2019-06-07 05:02:50 +0900

    Update homework1.c

--- lab11/homework1.c
+++ lab11/homework1.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 #include <time.h>
 
+void swap(int* a, int* b){
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void sort(int *a){
+    int least;
+    for(int i=0;i<10-1;i++){
+        least = i;
+        for(int j=i;j<10;j++){
+            if(a[j]<a[least])
+                least = j;
+        }
+        swap(&a[i],&a[least]);
+    }
+}
+
 int main(){
     int score[10];
     srand((unsigned)time(NULL));
@@ -10,17 +29,8 @@ int main(){
         score[i] = rand() % 100 + 1;
         printf("%d ",score[i]);
     }
-    int temp,least;
-    for(int i=0;i<10-1;i++){
-        least = i;
-        for(int j=i;j<10;j++){
-            if(score[j]<score[least])
-                least = j;
-        }
-        temp = score[i];
-        score[i] = score[least];
-        score[least] = temp;
-    }
+
+    sort(score);
 
     printf("\n최고점 : %d",score[9]);
     printf("\n최저점 : %d",score[0]);
